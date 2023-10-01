4eed3de8dc5ebe77bbbe46f52c5f8786f46c421b
fadhel <fadhelalqaidoom@icloud.com>
2023-10-01 14:36:47 +0300

    Full finish

--- src/App.js
+++ src/App.js
@@ -1,23 +1,59 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+  const [step, setStep] = useState(1);
+  const [count, setCount] = useState(0);
+  const [currentDate, setCurrentDate] = useState(new Date());
+
+  const incDate = () => {
+    const newDate = new Date();
+    newDate.setDate(currentDate.getDate() + step);
+    setCurrentDate(newDate);
+  };
+  const decDate = () => {
+    const newDate = new Date();
+    newDate.setDate(currentDate.getDate() - step);
+    setCurrentDate(newDate);
+  };
+
+  const stepInc = () => {
+    setStep(step + 1);
+  };
+  const stepDec = () => {
+    if (step > 1) {
+      setStep(step - 1);
+    }
+  };
+  const countInc = () => {
+    setCount(count + step);
+    incDate();
+  };
+  const countDec = () => {
+    setCount(count - step);
+    decDate();
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div>
+        <button onClick={stepDec}>-</button>
+        step: {step}
+        <button onClick={stepInc}>+</button>
+      </div>
+      <div>
+        <button onClick={countDec}>-</button>
+        count: {count}
+        <button onClick={countInc}>+</button>
+      </div>
+      <div>
+        {count === 0
+          ? 'Today is'
+          : count < 0
+          ? `${count * -1} day ago was `
+          : `${count} day from today is `}{' '}
+        {currentDate.toDateString()}
+      </div>
     </div>
   );
 }
