7318f17b57eef3f56c677a2fc51facef0bcd226b
fadhel <fadhelalqaidoom@icloud.com>
2023-10-02 16:07:13 +0300

    Upgrade the counter and add a reset button

--- src/App.js
+++ src/App.js
@@ -8,16 +8,36 @@ function App() {
   const date = new Date();
   date.setDate(date.getDate() + count);
 
+  function handleOnChangeStep(e) {
+    setStep(Number(e.target.value));
+  }
+  function handleOnChangecount(e) {
+    setCount(Number(e.target.value));
+  }
+  function handleOnClickReset() {
+    setCount(0);
+    setStep(1);
+  }
+
   return (
     <div>
       <div>
-        <button onClick={() => setStep((c) => c - 1)}>-</button>
-        step: {step}
-        <button onClick={() => setStep((c) => c + 1)}>+</button>
+        <input
+          type="range"
+          min="1"
+          max="10"
+          value={step}
+          onChange={handleOnChangeStep}
+        />
+        {step}
       </div>
       <div>
         <button onClick={() => setCount((c) => c - step)}>-</button>
-        count: {count}
+        <input
+          type="number"
+          value={count}
+          onChange={handleOnChangecount}
+        />
         <button onClick={() => setCount((c) => c + step)}>+</button>
       </div>
       <div>
@@ -28,6 +48,7 @@ function App() {
           : `${count} day from today is `}{' '}
         {date.toDateString()}
       </div>
+      <button onClick={handleOnClickReset}>Reset</button>
     </div>
   );
 }
